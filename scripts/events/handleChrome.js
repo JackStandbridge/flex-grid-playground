c8ea50e605d3deb531d31c9370ddeaedd010d933
Jack Standbridge <jack.s.standbridge@gmail.com>
2020-02-23 22:30:03 +0000

    animations easter eggs

--- scripts/events/handleChrome.js
+++ scripts/events/handleChrome.js
@@ -35,6 +35,40 @@ const handleChrome = () => {
 		document.addEventListener('click', clickOff);
 	});
 
+	const controls = document.querySelector('.controls');
+	const animations = [
+		{
+			name: 'escape',
+			duration: 4000
+		},
+		{
+			name: 'shuffle',
+			duration: 2000
+		},
+		{
+			name: 'roll',
+			duration: 2000
+		},
+	];
+
+	let counter = 0;
+	controls.addEventListener('click', ({ target }) => {
+		const animation = animations[counter % animations.length];
+		counter++;
+
+		[...target.parentNode.children].forEach((child, i) => {
+
+			setTimeout(() => {
+				child.classList.add(`control--${ animation.name }`);
+			}, i * 50);
+
+			setTimeout(() => {
+				child.classList.remove(`control--${ animation.name }`);
+			}, animation.duration + i * 50);
+
+		});
+	});
+
 }
 
 export default handleChrome;
