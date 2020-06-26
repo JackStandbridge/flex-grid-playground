e6fdac343095490884ebbfbb13bb62db67027e94
Jack Standbridge <jack.s.standbridge@gmail.com>
2020-06-26 10:26:26 +0100

    fix reset all bug

--- src/data/reducers/clearAll.js
+++ src/data/reducers/clearAll.js
@@ -1,27 +1,5 @@
 import initial from '../initial';
 
-const clearAll = state => {
-	const page = state.pages[state.page];
-	const toClear = page.child;
-	const toReset = [page.children, page.parent]
-
-	toClear.forEach(objId => {
-		state.styleObjects[objId].forEach(entryId => {
-			delete state.styleEntries[entryId];
-		});
-		state.styleObjects[objId] = [];
-	});
-
-	toReset.forEach(objId => {
-		if (objId in initial.styleObjects) {
-			state.styleObjects[objId] = initial.styleObjects[objId];
-		} else {
-			state.styleObjects[objId].forEach(entryId => {
-				delete state.styleEntries[entryId];
-			})
-		}
-	})
-
-};
+const clearAll = () => initial;
 
 export default clearAll;
